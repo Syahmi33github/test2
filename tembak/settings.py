18c8844653d6b6825f0a3b89d8a3c77da869f7f9
Syahmi33github <Syahmi33github@student.untan.ac.id>
2021-04-21 20:19:26 +0700

    Update settings.py

--- tembak/settings.py
+++ tembak/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'qx)+n6bb4699h#_pu_l@nia&h_54-xz-3b4zwqt2%)6*ca=tl8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','historyind.herokuapp.com']
 
 
 # Application definition
